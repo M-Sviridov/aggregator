4a91e5c09f2e8f3b187c46228fbef1142d294e10
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-11-12 14:49:53 +0700

    Implement chapter 3 lesson 1 RSS of the boot.dev project

--- handler.go
+++ handler.go
@@ -89,3 +89,17 @@ func handlerUsers(s *state, cmd command) error {
 
 	return nil
 }
+
+func handlerAgg(s *state, cmd command) error {
+	if len(cmd.arguments) != 0 {
+		return fmt.Errorf("%s does not take arguments\n", cmd.name)
+	}
+
+	rssFeed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if err != nil {
+		return fmt.Errorf("Error fetching feed: %w\n", err)
+	}
+
+	fmt.Println(rssFeed)
+	return nil
+}
---
+++ rss.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"encoding/xml"
+	"fmt"
+	"html"
+	"io"
+	"net/http"
+)
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return &RSSFeed{}, fmt.Errorf("Error when making request to feed url: %w\n", err)
+	}
+	req.Header.Set("User-Agent", "gator")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return &RSSFeed{}, fmt.Errorf("Error reading the RSS feed: %w\n", err)
+	}
+	defer resp.Body.Close()
+
+	rssFeed := RSSFeed{}
+	body, err := io.ReadAll(resp.Body)
+	err = xml.Unmarshal(body, &rssFeed)
+	if err != nil {
+		return &RSSFeed{}, fmt.Errorf("Error unmarshaling XML data: %w\n", err)
+	}
+
+	rssFeed.Channel.Title = html.UnescapeString(rssFeed.Channel.Title)
+	rssFeed.Channel.Description = html.UnescapeString(rssFeed.Channel.Description)
+
+	for _, item := range rssFeed.Channel.Item {
+		item.Title = html.UnescapeString(item.Title)
+		item.Description = html.UnescapeString(item.Description)
+	}
+
+	return &rssFeed, nil
+}
