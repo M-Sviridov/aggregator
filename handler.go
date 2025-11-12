bdaac7d50c3fc4c875d5d2162b27befeca81ce89
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-11-12 09:46:41 +0700

    Implement chapter 2 lesson 4 reset function of the boot.dev project

--- handler.go
+++ handler.go
@@ -55,3 +55,16 @@ func handlerRegister(s *state, cmd command) error {
 
 	return nil
 }
+
+func handlerReset(s *state, cmd command) error {
+	if len(cmd.arguments) != 0 {
+		return fmt.Errorf("%s does not take arguments\n", cmd.name)
+	}
+
+	err := s.db.DeleteAllUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("Error in deleting all users: %w", err)
+	}
+
+	return nil
+}
