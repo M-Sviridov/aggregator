9395a6d8af569094791f1c380ba12b2911887f41
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-11-13 10:33:11 +0700

    Fix wrong type STRING to correct TEXT

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -4,7 +4,7 @@ CREATE TABLE feeds (
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL,
   name TEXT UNIQUE NOT NULL,
-  url STRING UNIQUE NOT NULL,
+  url TEXT UNIQUE NOT NULL,
   user_id UUID NOT NULL,
   FOREIGN KEY(user_id) 
     REFERENCES users(id)
