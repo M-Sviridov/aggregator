30383fd606fe99184df73e5f706ffab84f0ed021
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-11-15 17:20:18 +0700

    Remove extra duplicate handler

--- handler_agg.go
+++ handler_agg.go
@@ -5,19 +5,6 @@ import (
 	"fmt"
 )
 
-func handlerReset(s *state, cmd command) error {
-	if len(cmd.arguments) != 0 {
-		return fmt.Errorf("%s does not take arguments\n", cmd.name)
-	}
-
-	err := s.db.DeleteAllUsers(context.Background())
-	if err != nil {
-		return fmt.Errorf("Error in deleting all users: %w", err)
-	}
-
-	return nil
-}
-
 func handlerAgg(s *state, cmd command) error {
 	if len(cmd.arguments) != 0 {
 		return fmt.Errorf("%s does not take arguments\n", cmd.name)
