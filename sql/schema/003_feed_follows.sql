dfc2adcfd5fe460e10766ed135a366ab32da51ef
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-11-15 18:26:48 +0700

    Simplify table creation query

--- sql/schema/003_feed_follows.sql
+++ sql/schema/003_feed_follows.sql
@@ -3,18 +3,9 @@ CREATE TABLE feed_follows (
   id UUID PRIMARY KEY,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL,
-  user_id UUID NOT NULL,
-  feed_id UUID NOT NULL,
-  CONSTRAINT feed_follows_user_id_fk
-    FOREIGN KEY(user_id) 
-    REFERENCES users(id)
-    ON DELETE CASCADE,
-  CONSTRAINT feed_follows_feed_id_fk
-    FOREIGN KEY(feed_id) 
-    REFERENCES feeds(id)
-    ON DELETE CASCADE,
-  CONSTRAINT feed_follows_user_id_feed_id_unique
-    UNIQUE(user_id, feed_id)
+  user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE,
+  UNIQUE(user_id, feed_id)
 );
 
 -- +goose Down
