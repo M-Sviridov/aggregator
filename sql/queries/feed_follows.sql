28be141ecc38c3e2064f1d7629d9d4de7edebf4d
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-11-15 18:19:16 +0700

    Add query to get user feed follows

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -1,4 +1,4 @@
--- name: CreateFeedFollow :many
+-- name: CreateFeedFollow :one
 WITH inserted_feed_follow AS (
   INSERT INTO feed_follows(id, created_at, updated_at, user_id, feed_id)
   VALUES (
@@ -18,3 +18,11 @@ FROM inserted_feed_follow
 INNER JOIN users ON inserted_feed_follow.user_id = users.id
 INNER JOIN feeds ON inserted_feed_follow.feed_id = feeds.id;
 
+-- name: GetFeedFollowsForUser :many
+SELECT
+  feeds.name AS feed_name,
+  users.name AS user_name
+FROM feed_follows
+INNER JOIN users ON feed_follows.user_id = users.id
+INNER JOIN feeds ON feed_follows.feed_id = feeds.id
+WHERE feed_follows.user_id = $1;
